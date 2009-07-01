25c8b51a066879c443485015977c24b0b44f835d
wear <wear63659220@gmail.com>
2009-07-01 13:35:07 +0800

    add sendmail

--- config/environment.rb
+++ config/environment.rb
@@ -34,9 +34,15 @@ Rails::Initializer.run do |config|
    config.active_record.observers = :user_observer     
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = 'UTC'
+  config.time_zone = 'Beijing'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   config.i18n.default_locale = 'zh-CN'
-end
+end  
+
+ActionMailer::Base.delivery_method = :sendmail
+ActionMailer::Base.sendmail_settings = {
+    :location       => '/usr/sbin/sendmail',
+    :arguments      => '-i -t'
+}
