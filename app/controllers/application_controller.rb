28f350d696ad3d4459548fb59d61ce8b2fb29dbb
wear <wear63659220@gmail.com>
2009-07-20 21:38:13 +0800

    more css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
   include AuthenticatedSystem
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  
   
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password            
